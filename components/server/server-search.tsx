298a587aa2e1b458e15e8507b69d36d2184d3e10
sandeep-v1404 <valpasanisandeep@gmail.com>
2023-11-11 01:11:07 +0530

    feat: added server searchbar

---
+++ components/server/server-search.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { Command, CreditCard, Search, Settings, User } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+interface ServerSearchProps {
+  searchData: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[]
+      | undefined;
+  }[];
+}
+
+export const ServerSearch: React.FC<ServerSearchProps> = ({ searchData }) => {
+  const [open, setOpen] = useState(false);
+
+  const router = useRouter();
+  const params = useParams();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const onClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+    if (type === "member") {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+    }
+    if (type === "channel") {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex 
+      items-center gap-x-2 w-full hover:bg-zinc-700/10
+       dark:hover:bg-zinc-700/50 transition"
+      >
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p
+          className="font-semibold text-sm text-zinc-500
+         dark:text-zinc-400 group-hover:text-zinc-600
+          dark:group-hover:text-zinc-300 transition"
+        >
+          Search
+        </p>
+        <kbd
+          className="pointer-events-none inline-flex h-5 
+        select-none items-center gap-1 rounded 
+        border bg-muted px-1.5 font-mono text-[10px] 
+        font-medium text-muted-foreground ml-auto"
+        >
+          <span className="text-xs">
+            <Command className="w-3 h-3 text-zinc-500 dark:text-zinc-400" />
+          </span>
+          K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {searchData?.map(({ label, type, data }) => {
+            if (!data?.length) return null;
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data?.map(({ id, name, icon }) => {
+                  return (
+                    <CommandItem
+                      key={id}
+                      onSelect={() => onClick({ id, type })}
+                    >
+                      {icon}
+                      <span>{name}</span>
+                    </CommandItem>
+                  );
+                })}
+              </CommandGroup>
+            );
+          })}
+
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
